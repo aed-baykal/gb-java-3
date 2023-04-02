d2f2ccc656263d189dc1c9353519717bee3f780f
Andrey Dubovitskiy <aed_baykal@mail.ru>
2023-04-02 21:14:12 +0300

    добавил lesson3

---
+++ src/main/java/ru/gb/java.three/lesson3/Main.java
@@ -0,0 +1,29 @@
+package ru.gb.java.three.lesson3;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+
+    private static int count = 0;
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void getIncrement() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        Player player1 = new Player("ping", lock);
+        Player player2 = new Player("pong", lock);
+        while (count < 10) {
+            player1.run();
+            System.out.println(getCount());
+            player2.run();
+            System.out.println(getCount());
+        }
+    }
+}
